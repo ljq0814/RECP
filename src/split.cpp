563bf18c63da39401fa50adb1fab0bfeb6ee37b9
Gareth Li <ljq0814nana@gmail.com>
2025-05-13 11:19:27 +0800

    Update split.cpp

--- src/split.cpp
+++ src/split.cpp
@@ -1,17 +1,6 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-
-
 // [[Rcpp::export]]
 SEXP getWithin( SEXP alpha_, SEXP X_ ){
   BEGIN_RCPP
@@ -162,10 +151,3 @@ SEXP split_re1( SEXP s_, SEXP e_, SEXP D_, SEXP min_size_ ){
 
   END_RCPP
 }
-
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically
-// run after the compilation.
-//
-
